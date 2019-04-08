ba22657e618d39aca7284d4c731ef38446269ed7
zhangzhendong <1280209233@qq.com>
2019-04-08 11:18:40 +0800

    更新项目

--- app/src/main/java/fanmie/com/fanmie/MainActivity.java
+++ app/src/main/java/fanmie/com/fanmie/MainActivity.java
@@ -2,12 +2,40 @@ package fanmie.com.fanmie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etAccount;
+    private EditText etPassword;
+    private TextView tvBind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        tvBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String count = etAccount.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+
+
+            }
+        });
+    }
+
+    private void initView() {
+        etAccount = (EditText) findViewById(R.id.etAccount);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        tvBind = (TextView) findViewById(R.id.tvBind);
     }
 }
